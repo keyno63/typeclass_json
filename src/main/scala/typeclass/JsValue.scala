8429df3d2465920f8656be536d6e47f1de30c8c9
Keyno <kofujiw@gmail.com>
2020-03-20 23:55:37 +0900

    add function to json trait

--- src/main/scala/typeclass/JsValue.scala
+++ src/main/scala/typeclass/JsValue.scala
@@ -1,9 +1,35 @@
 package typeclass
 
-trait JsValue
-case class JsString(value: String) extends JsValue
-case class JsNumber(value: Double) extends JsValue
-case class JsBoolean(value: Boolean) extends JsValue
-case class JsObject(value: Map[String, JsValue]) extends JsValue
-case class JsArray(value: Vector[JsValue]) extends JsValue
-case object JsNull extends JsValue
+trait JsValue {
+  def \(key: String): JsPath
+  def \(key: Int): JsPath
+}
+case class JsString(value: String) extends JsValue {
+  override def \(key: String): JsPath      = JsPath(None)
+  override def \(key: Int): JsPath         = JsPath(None)
+}
+case class JsNumber(value: Double) extends JsValue {
+  override def \(key: String): JsPath      = JsPath(None)
+  override def \(key: Int): JsPath         = JsPath(None)
+}
+case class JsBoolean(value: Boolean) extends JsValue {
+  override def \(key: String): JsPath      = JsPath(None)
+  override def \(key: Int): JsPath         = JsPath(None)
+}
+case class JsObject(value: Map[String, JsValue]) extends JsValue {
+  override def \(key: String): JsPath      = JsPath(value.get(key))
+  override def \(key: Int): JsPath         = JsPath(None)
+}
+case class JsArray(value: Vector[JsValue]) extends JsValue {
+  override def \(key: String): JsPath      = JsPath(None)
+  override def \(key: Int): JsPath         = JsPath(value.lift(key))
+}
+case object JsNull extends JsValue {
+  override def \(key: String): JsPath      = JsPath(None)
+  override def \(key: Int): JsPath         = JsPath(None)
+}
+
+case class JsPath(getOption: Option[JsValue]) {
+  def \(key: String): JsPath = JsPath(getOption.map(_ \ key).flatMap(_.getOption))
+  def \(key: Int): JsPath    = JsPath(getOption.map(_ \ key).flatMap(_.getOption))
+}
