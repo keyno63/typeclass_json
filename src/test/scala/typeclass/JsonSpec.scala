dccc37f47f6e63af997caa84d535f02409222fec
Keyno <kofujiw@gmail.com>
2020-03-24 23:50:29 +0900

    add test

--- src/test/scala/typeclass/JsonSpec.scala
+++ src/test/scala/typeclass/JsonSpec.scala
@@ -1,5 +1,112 @@
 package typeclass
 
-class JsonSpec {
+import org.scalatest.{FlatSpec, Matchers}
 
+class JsonSpec extends FlatSpec with Matchers {
+
+  val template: JsValue = JsObject(
+    Map(
+      "name" -> JsString("WaterShip Down"),
+      "location" -> JsObject(
+        Map(
+          "lat" -> JsNumber(51.235685),
+          "lng" -> JsNumber(-1.309197)
+        )
+      ),
+      "residents" -> JsArray(
+        Vector(
+          JsObject(
+            Map(
+              "name" -> JsString("Fiver"),
+              "age"  -> JsNumber(4),
+              "role" -> JsNull
+            )
+          ),
+          JsObject(
+            Map(
+              "name" -> JsString("Bigwig"),
+              "age"  -> JsNumber(6),
+              "role" -> JsString("Owsla")
+            )
+          )
+        )
+      )
+    )
+  )
+
+  "JsValue" should "build" in {
+
+    template.isInstanceOf[JsValue] shouldEqual true
+
+  }
+
+  "JsValue" should "traverse (1)" in {
+
+    val result: Option[JsValue] = (template \ "location" \ "lng").getOption
+    val answer: Option[JsValue] = Some(JsNumber(-1.309197))
+
+    result shouldEqual answer
+
+  }
+
+  "JsValue" should "traverse (2)" in {
+
+    val result: Option[JsValue] = (template \ "residents" \ 1 \ "role").getOption
+    val answer: Option[JsValue] = Some(JsString("Owsla"))
+
+    result shouldEqual answer
+
+  }
+
+  "JsValue" should "traverse (3)" in {
+
+    val result: Option[JsValue] = (template \ "residents" \ 0 \ "role").getOption
+    val answer: Option[JsValue] = Some(JsNull)
+
+    result shouldEqual answer
+
+  }
+
+  "JsValue" should "traverse (4)" in {
+
+    val result: Option[JsValue] = (template \ "name" \ "wrongkey!").getOption
+    val answer: Option[JsValue] = None
+
+    result shouldEqual answer
+
+  }
+
+  "JsPath" should "convert value" in {
+
+    val reuslt: Option[String] = (template \ "name").as[String]
+    val answer                 = Some("WaterShip Down")
+
+    reuslt shouldEqual answer
+
+  }
+
+  "JsValue" should "get value throws exception (1)" in {
+
+    val result: JsValue = template("residents")(0)("name")
+    val answer          = JsString("Fiver")
+    result shouldEqual answer
+
+  }
+
+  "JsValue" should "get value throws exception (2)" in {
+
+    val json: JsValue = JsString("foo")
+    intercept[NoSuchElementException] {
+      json("bar")
+    }
+
+  }
+
+  "JsValue" should "get value throws exception (3)" in {
+
+    intercept[NoSuchElementException] {
+      (template \ "location")("latitude")
+    }
+
+  }
 }
